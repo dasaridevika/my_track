e19b4c99ea2733f12653ff9ce724e0eb46fe3df4
DasariDevika <165867121+dasaridevika@users.noreply.github.com>
2026-07-27 16:40:13 +0530

    Change API_URL to local development endpoint

--- frontend/app.py
+++ frontend/app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 # -----------------------------
 API_URL = os.getenv(
     "API_URL",
-    "https://grateful-caring-production-d098.up.railway.app/crawl"
+    "http://127.0.0.1:8000/crawl"
 )
 # -----------------------------
 # UI
