6acd2b2ae11b1a1a48a4520df69b22a5e1a6a54f
DasariDevika <165867121+dasaridevika@users.noreply.github.com>
2026-07-27 18:48:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 uvicorn backend.main:app \
-    --host 127.0.0.1 \
-    --port 8000 &
+  --host 127.0.0.1 \
+  --port 8000 &
 streamlit run frontend/app.py \
-    --server.port 8501 \
-    --server.address 127.0.0.1 &
-nginx -c $(pwd)/nginx.conf -g "daemon off;"
+  --server.port 8501 \
+  --server.address 127.0.0.1 &
+envsubst '${PORT}' < nginx.conf > /tmp/nginx.conf
+nginx -c /tmp/nginx.conf -g "daemon off;"
