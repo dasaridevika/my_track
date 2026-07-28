a45334e4805f5107f30a650b7c5d7f7fb1b3be9f
DasariDevika <165867121+dasaridevika@users.noreply.github.com>
2026-07-28 10:30:33 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-uvicorn backend.main:app \
-  --host 127.0.0.1 \
-  --port 8000 &
-streamlit run frontend/app.py \
-  --server.port 8501 \
-  --server.address 127.0.0.1 &
-envsubst '${PORT}' < nginx.conf > /tmp/nginx.conf
-nginx -c /tmp/nginx.conf -g "daemon off;"
